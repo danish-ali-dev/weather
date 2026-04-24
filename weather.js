fa32d1881e10a3a21b52e9474a018d9c0db62dc7
danish-ali-dev <chdanisha897@gmail.com>
2026-04-24 16:04:21 +0530

    Deployed my weather project to GitHub. This is the initial commit with all the necessary files for the project, including the HTML, CSS, JavaScript, and an icon image. The project is now live and can be accessed through the GitHub repository.

---
+++ weather.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com',
+		'Content-Type': 'application/json'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
