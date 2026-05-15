85ed3ae4ddbe0e7027075ffc983068a0b7cd4fba
danish-ali-dev <chdanisha897@gmail.com>
2026-05-16 01:08:05 +0530

    new

--- weather.js
+++ weather.js
@@ -1,16 +1,19 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const url = 'https://api.api-ninjas.com/v1/weather?city=srinagar';
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com',
+		'X-Api-Key': 'yWESxrmBKRgsneLp7rfF1lPPGWPwJNB1fwyFcjeo',
 		'Content-Type': 'application/json'
 	}
 };
+async function getWeather() {
 
 try {
 	const response = await fetch(url, options);
-	const result = await response.text();
+	const result = await response.json();
 	console.log(result);
 } catch (error) {
 	console.error(error);
 }
+}
+getWeather();
